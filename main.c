68b1169a275ea625be562465117f1057a33cb7e7
SofiaBala <99535503+SofiaBala@users.noreply.github.com>
2022-02-28 01:39:21 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,42 +2,32 @@
 #include <stdio.h> 
 #include <time.h> 
 #include <stdlib.h> 
-extern char* tzname[];
+#define ERROR_PUTENV -1
+#define ERROR_TIME -1
 
 int main() {
 	time_t now;
-	struct tm* sp;
-	char *ct;
-	
+	char *ctDate;
+
 	int area = putenv("TZ=America/Los_Angeles");
-	if (area != 0){
-		perror("Error");
-		exit(1);
-	}
-	
-	time_t = time(&now);
-	if (t == -1){
-            perror("Error");
-            exit(1);
-    	}
-
-	ct = ctime(&now);
-	if (ct == NULL){
-            perror("Error");
-            exit(1);
-    	}
-
-    	printf("%s", ct);
-
-	sp = localtime(&now);
-	if (sp == NULL){
-		perror("Error");
+	if (area == ERROR_PUTENV){
+		perror("Error putenv");
 		exit(1);
 	}
 
-	printf("%d/%d/%02d %d:%02d %s\n", 
-	       sp->tm_mon + 1, sp->tm_mday, 
-	       sp->tm_year -100, sp->tm_hour, 
-	       sp->tm_min, tzname[sp->tm_isdst]);
+	int t = time(&now);
+	if (t == ERROR_TIME){
+            perror("Error time");
+            exit(1);
+    	}
+
+	ctDate = ctime(&now);
+	if (ctDate == NULL){
+            perror("Error ctime");
+            exit(1);
+    	}
+
+    	printf("%s", ctDate);
+
 	exit(0);
 }
