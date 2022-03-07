40599411f3577aa9ed3ab513820600c994ecc026
SofiaBala <99535503+SofiaBala@users.noreply.github.com>
2022-03-07 18:17:38 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <sys/types.h>
 #include <stdio.h> 
+#include <sys/types.h>
+#include <stdio.h> 
 #include <time.h> 
-#include <stdlib.h> 
+#include <stdlib.h>
+ 
 #define ERROR_PUTENV -1
 #define ERROR_TIME -1
+#define PUTENV_EXIT_ERROR 1
+#define TIME_EXIT_ERROR 2
+#define CTIME_EXIT_ERROR 3
 
 int main() {
 	time_t now;
@@ -12,22 +18,22 @@ int main() {
 	int area = putenv("TZ=America/Los_Angeles");
 	if (area == ERROR_PUTENV){
 		perror("Error putenv");
-		exit(1);
+		exit(PUTENV_EXIT_ERROR);
 	}
 
 	int t = time(&now);
 	if (t == ERROR_TIME){
             perror("Error time");
-            exit(1);
+            exit(TIME_EXIT_ERROR);
     	}
 
 	ctDate = ctime(&now);
 	if (ctDate == NULL){
             perror("Error ctime");
-            exit(1);
+            exit(CTIME_EXIT_ERROR);
     	}
 
     	printf("%s", ctDate);
 
-	exit(0);
+	return(0);
 }
