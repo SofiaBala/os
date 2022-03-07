e49db3a798bb5507044597eceddbd2ceebd4122f
SofiaBala <99535503+SofiaBala@users.noreply.github.com>
2022-03-07 20:21:24 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include <sys/types.h>
-#include <stdio.h> 
-#include <time.h> 
-#include <stdlib.h>
+#include <sys/types.h>//Содержит time_t для переносимости программы на другое устройство
+#include <stdio.h> //стандартная библ ввода выв
+#include <time.h> //для функц работы со врем
+#include <stdlib.h>//для функц strlen
  
 #define ERROR_PUTENV -1
 #define ERROR_TIME -1
@@ -10,22 +10,29 @@
 #define CTIME_EXIT_ERROR 3
 
 int main() {
-	time_t now;
+	time_t now;//Определяет переменную now типа time_t
 	char *ctDate;
 
-	int area = putenv("TZ=America/Los_Angeles");
+	int area = putenv("TZ=America/Los_Angeles");//Функция putenv добавляет новые переменные окружения или мо-
+        						// дифицирует значения существующих переменных окружения
+
 	if (area == ERROR_PUTENV){
 		perror("Error putenv");
 		exit(PUTENV_EXIT_ERROR);
 	}
 
-	int t = time(&now);
+	int t = time(&now);//Вызывается time(2). Время в секундах от 00:00:00 UTC 1 января 1970 сохраняется в
+				//переменной now
+
 	if (t == ERROR_TIME){
             perror("Error time");
             exit(TIME_EXIT_ERROR);
     	}
 
-	ctDate = ctime(&now);
+	ctDate = ctime(&now);//Библиотечная функция ctime(3C) преобразует календарное время в ASCII-строку
+				//формата date(1). Адрес, возвращенный этой функцией, используется в качестве параметра
+				//printf для печати ASCII-строки
+
 	if (ctDate == NULL){
             perror("Error ctime");
             exit(CTIME_EXIT_ERROR);
