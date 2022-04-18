f1c27af7286b4355f072d3ecc276f53a9f92ca5b
SofiaBala <99535503+SofiaBala@users.noreply.github.com>
2022-04-18 19:48:42 +0700

    Update lab21.c

--- lab21.c
+++ lab21.c
@@ -11,37 +11,31 @@
 int flag = 1;
 int count_sig;
 
-int main(){
-	void sigcatch(int);
-
-	void* signals = signal(SIGINT, sigcatch);
-	if(signals == SIG_ERR){
-		perror("signal");
-        	return ERROR;
-	}
-	signals = signal(SIGQUIT, sigcatch);
-	if(signals == SIG_ERR){
-		perror("signal");
-        	return ERROR;
-	}
-
-	while(flag){
-        	pause();
-    }
-}
-
 void sigcatch(int sig)
 {
 	if (sig == SIGQUIT) {
 		printf("\n bell was rung %d times\n", count_sig);
 		exit(0);
 	}
-	printf("%c\n", BEL);
+	
 	count_sig++;
-	void* signals = signal(sig, sigcatch);
+	printf("%c\n", BEL);
+}
+
+int main()
+{
+	void* signals = sigset(SIGINT, sigcatch);
 	if(signals == SIG_ERR){
-		perror("signal");
-        	exit(ERROR);
+		perror("sigset");
+        	return ERROR;
+	}
+	signals = sigset(SIGQUIT, sigcatch);
+	if(signals == SIG_ERR){
+		perror("sigset");
+        	return ERROR;
 	}
 
+	while(flag){
+        	pause();
+    	}
 }
