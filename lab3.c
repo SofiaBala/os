19f6ff1f031e8a9514e61457cae9eeaf3ca93756
SofiaBala <99535503+SofiaBala@users.noreply.github.com>
2022-04-25 00:11:09 +0700

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -2,40 +2,45 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define ERROR 1
+#define SETUID_ERROR 2
+#define ERROR -1
+#define FOPEN_ERROR 3
+#define FCLOSE_ERROR 4
+
+int print_id(){
+	uid_t uid = getuid();
+	uid_t euid = geteuid();
+
+	printf("Real user id: %d, effective user id: %d\n", uid, euid);	
+	
+	FILE* file = fopen("text.txt", "r");
+	if (file == NULL) {
+		perror("File");
+		return FOPEN_ERROR;
+	}
+	int close = fclose(file);
+	if(close == EOF){
+		perror("fclose");
+		return FCLOSE_ERROR;
+	}
+	return(0);
+}
 
 int main()
 {	
-	printf("Real user id: %d, effective user id: %d\n", getuid(), geteuid());// Печать реального и эффективного идентификатора пользователя
-	
-	FILE* file = fopen("text.txt", "r");
-	// NULL - код ошибки, если файл не удалось открыть
-	if (file == NULL) {
-		perror("File (first try)");
-		exit(1);
-	}
-	else {
-		printf("File opened (first try)\n");
-		fclose(file);
-	}
+	int id = print_id();
+	if (id == FCLOSE_ERROR){
+		return FCLOSE_ERROR;
+	}	
 
- 	int effect = setuid(getuid());// Устанавливаем эффективный идентификатор равным реальному
+ 	int effect = setuid(getuid());
  	if (effect == ERROR) {
  		perror("setuid");
- 		return ERROR;
+ 		return SETUID_ERROR;
  	}
-
-	printf("New real user id: %d, new effective user id: %d\n", getuid(), geteuid());// Печать нового реального и нового эффективного идентификатора пользователя
-
-	FILE* ffile = fopen("text.txt", "r");
-	// NULL - код ошибки, если файл не удалось открыть
-	if (ffile == NULL) {
-		perror("File (second try)");
-		exit(2);
-	}
-	else {
-		printf("File opened (second try)\n");
-		fclose(ffile);
+	id = print_id();
+	if (id == FCLOSE_ERROR){
+		return FCLOSE_ERROR;
 	}
 	return(0);
 }
