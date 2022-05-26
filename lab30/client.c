bc26959a7d891a9dc727cccaa6a68a3c0beda971
SofiaBala <99535503+SofiaBala@users.noreply.github.com>
2022-05-26 18:17:01 +0700

    Update client.c

--- lab30/client.c
+++ lab30/client.c
@@ -15,81 +15,79 @@
 #define ERROR_READ -1
 #define ERROR_WRITE -1
 #define ERROR_CLOSE -1
+#define ERROR 1
+#define END_OF_FILE 0
+#define TRUE 1
 
 int clientfd;
+void catchsignal(int sig){}
 
-int f_close(int clientfd){
+int close_socket(int clientfd){
     int num_close = close(clientfd);
     if(num_close == ERROR_CLOSE){
 	perror("error close");
-	return ERROR_CLOSE;
+	return ERROR;
     }
 }
-int f_unlink(const char *name){
+
+int unlink_file(const char *name){
     if(unlink(name) == ERROR_UNLINK){
 	perror("error unlink");
 	return ERROR_UNLINK;
     }
 }
 
-void sigcatch(int sig) {
-    if(sig == SIGINT) {
-        f_close(clientfd);
-        f_unlink(FILENAME);
-	exit(1);
-    }
-}
+int write_info(int clientfd){
+	
+    int num_read, num_write;
+    char buffer[BUFSIZ];
 
-int ch_socket(int domain, int type, int protocol) {
-    int res = socket(domain, type, protocol);
-    if(res == ERROR_SOCKET) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-    return res;
-}
+        while(TRUE) {
+	num_read = read(STDIN_FILENO, buffer, BUFSIZ);
+	if(num_read == ERROR_READ){
+	    perror("error read");
+	    return ERROR;
 
-void ch_connect(int clientfd, struct sockaddr *addr, socklen_t addrlen) {
-    if(connect(clientfd, addr, addrlen) == ERROR_CONNECT) {
-        perror("connect failed");
-        exit(EXIT_FAILURE);
+	}
+	if (num_read == END_OF_FILE) break;
+	num_write = write(clientfd, buffer, num_read);
+	if(num_write == ERROR_WRITE){
+	    perror("error write");
+	    return ERROR;
+	}
     }
+    return 0;
 }
-
 
 int main() {
-    struct sockaddr_un c_addr;
-    signal(SIGINT, sigcatch);
+    void* signal_result = signal(SIGPIPE, catchsignal);
+    if (signal_result == SIG_ERR){
+        perror("working signal");
+        return ERROR;
+    }
 
-    clientfd = ch_socket(AF_UNIX, SOCK_STREAM, 0);
+    struct sockaddr_un c_addr;
+    clientfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if(clientfd == ERROR_SOCKET) {
+        perror("socket failed");
+        return ERROR;
+    }
 
     memset(&c_addr, 0, sizeof(struct sockaddr_un));
     c_addr.sun_family = AF_UNIX;
     strncpy(c_addr.sun_path, FILENAME, sizeof(c_addr.sun_path) - 1);
 
-    ch_connect(clientfd, (struct sockaddr*)&c_addr, sizeof(c_addr));
-
-    int num_read, num_write;
-    char buffer[BUFSIZ];
+    int connected = connect(clientfd, (struct sockaddr*)&c_addr, sizeof(c_addr));
+    if(connected == ERROR_CONNECT) {
+        perror("connect failed");
+        return ERROR;
+    }
     
-    while((num_read = read(1, buffer, BUFSIZ)) > 0) {
-	if(num_read == ERROR_READ){
-	    perror("error read");
-	    return ERROR_READ;
-	}
-	num_write = write(clientfd, buffer, num_read);
-	if(num_write == ERROR_WRITE){
-	    perror("error write");
-	    return ERROR_WRITE;
-	}
-        if(num_write != num_read) {
-            printf("writing failed");
-            f_close(clientfd);
-            unlink(FILENAME);
-            exit(1);
-        }
-    } 
-    f_close(clientfd);   
-    f_unlink(FILENAME);
+    int write = write_info(clientfd);
+    if(write == ERROR){
+	close_socket(clientfd);
+	return ERROR;
+    }
+    close_socket(clientfd);   
     return 0;
 }
